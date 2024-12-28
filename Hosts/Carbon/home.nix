f21e892b3c08359481214b84a4633d2cd846b7e3
CryptixMC <90222850+CryptixMC@users.noreply.github.com.>
2024-12-28 05:25:55 +0000

    started modules

---
+++ Hosts/Carbon/home.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "cryptix";
+  home.homeDirectory = "/home/cryptix";
+
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = [
+
+  ];
+
+  home.file = {
+
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
--- flake.nix
+++ flake.nix
@@ -11,12 +11,21 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      carbon = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./Hosts/Carbon/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+      silicon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Use the 'path' function to ensure it's included correctly
+          ./Hosts/Silicon/configuration.nix
+        ];
+      };
     };
   };
 }
