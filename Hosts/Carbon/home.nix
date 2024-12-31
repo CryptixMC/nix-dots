b431ebfc614e580a2c95457a3168227baf3190be
CryptixMC <90222850+CryptixMC@users.noreply.github.com.>
2024-12-31 03:44:04 +0000

    getting setup

--- Hosts/Carbon/home.nix
+++ Hosts/Carbon/home.nix
@@ -4,20 +4,61 @@
   home.username = "cryptix";
   home.homeDirectory = "/home/cryptix";
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
   home.packages = [
-
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.gcc
+    pkgs.bottom
+    pkgs.eza
+    pkgs.disfetch
+    pkgs.fd
+    pkgs.onefetch
   ];
 
   home.file = {
 
   };
 
+  programs = {
+    git = {
+      enable = true;
+      userName = "cryptix";
+      userEmail = "cryptixmc@proton.me";
+      aliases = {
+        pu = "push";
+        co = "checkout";
+        cm = "commit";
+      };
+    };
+  
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      enableCompletion = true;
+      shellAliases = {
+        ls = "eza --icons -l -T -L=2";
+	cat = "bat";
+	fd = "fd -Lu";
+	fetch = "disfetch";
+        gitfetch = "onefetch";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" ];
+        theme = "robbyrussell";
+      };
+      envExtra = ''
+        fetch
+        eval "$(direnv hook zsh)"  
+      '';
+    };
+  };
+
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
-  # Let Home Manager install and manage itself.
+  home.stateVersion = "24.05";
+
   programs.home-manager.enable = true;
 }
--- flake.nix
+++ flake.nix
@@ -3,29 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    #stylix.url = "github:danth/stylix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      carbon = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./Hosts/Carbon/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-      silicon = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Use the 'path' function to ensure it's included correctly
-          ./Hosts/Silicon/configuration.nix
-        ];
+  outputs = { self, nixpkgs, ... }@inputs: 
+  let 
+    system = "x86_x64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
       };
     };
+  in
+  {
+  nixosConfigurations = {
+    carbon = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs system;};
+      modules = [
+        ./Hosts/Carbon/configuration.nix
+        inputs.home-manager.nixosModules.default
+	#stylix.nixosModules.stylix
+      ];
+    };
+    silicon = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./Hosts/Silicon/configuration.nix
+      ];
+    };
+  };
   };
 }
